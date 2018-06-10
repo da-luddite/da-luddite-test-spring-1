08bec8500a2d6a6edf87bc101cae6be2999ba493
Eric Hamlin <da_luddite@yahoo.com>
2018-06-10 15:27:36 -0700

    code

--- src/main/java/com/odiousenterprises/testspring/DaLudditeTestSpring1Application.java
+++ src/main/java/com/odiousenterprises/testspring/DaLudditeTestSpring1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DaLudditeTestSpring1Application {
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DaLudditeTestSpring1Application.class, args);
