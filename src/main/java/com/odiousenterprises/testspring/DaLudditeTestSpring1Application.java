bd88e542625ae37d2640fc1ac166c5cf39248a63
da-luddite <da_luddite@yahoo.com>
2018-06-10 20:39:27 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/odiousenterprises/testspring/DaLudditeTestSpring1Application.java
@@ -0,0 +1,12 @@
+package com.odiousenterprises.testspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaLudditeTestSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaLudditeTestSpring1Application.class, args);
+	}
+}
---
+++ src/test/java/com/odiousenterprises/testspring/DaLudditeTestSpring1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.odiousenterprises.testspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaLudditeTestSpring1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
